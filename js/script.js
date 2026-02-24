f75bc7426a216522088139f160e3b56b8ccc5393
Tushar Kumar <tusharkumar@Tushar-ka-laipatopa.local>
2026-02-24 15:11:35 +0530

    [NEW] images all the brick work and new build

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-(function($) {
+(function ($) {
 	"use strict";
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
-		if($('.preloader').length){
+		if ($('.preloader').length) {
 			$('.preloader').delay(200).fadeOut(500);
 		}
 	}
-	
+
 	//Update Header Style and Scroll to Top
 	function headerStyle() {
-		if($('.main-header').length){
+		if ($('.main-header').length) {
 			var windowpos = $(window).scrollTop();
 			var siteHeader = $('.main-header');
 			var scrollLink = $('.scroll-to-top');
@@ -22,32 +22,32 @@
 			}
 		}
 	}
-	
+
 	headerStyle();
-	
+
 	//Submenu Dropdown Toggle
-	if($('.main-header .navigation li.dropdown ul').length){
+	if ($('.main-header .navigation li.dropdown ul').length) {
 		$('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fa fa-angle-down"></span></div>');
-		
+
 		//Dropdown Button
-		$('.main-header .navigation li.dropdown .dropdown-btn').on('click', function() {
+		$('.main-header .navigation li.dropdown .dropdown-btn').on('click', function () {
 			$(this).prev('ul').slideToggle(500);
 		});
-		
+
 		//Disable dropdown parent link
-		$('.navigation li.dropdown > a').on('click', function(e) {
+		$('.navigation li.dropdown > a').on('click', function (e) {
 			e.preventDefault();
 		});
 	}
-		
-	if($('.project-tab').length){
-		$('.project-tab .product-tab-btns .p-tab-btn').on('click', function(e) {
+
+	if ($('.project-tab').length) {
+		$('.project-tab .product-tab-btns .p-tab-btn').on('click', function (e) {
 			e.preventDefault();
 			var target = $($(this).attr('data-tab'));
-			
-			if ($(target).hasClass('actve-tab')){
+
+			if ($(target).hasClass('actve-tab')) {
 				return false;
-			}else{
+			} else {
 				$('.project-tab .product-tab-btns .p-tab-btn').removeClass('active-btn');
 				$(this).addClass('active-btn');
 				$('.project-tab .p-tabs-content .p-tab').removeClass('active-tab');
@@ -55,282 +55,298 @@
 			}
 		});
 	}
-	
+
 	// Scroll to a Specific Div
-	if($('.scroll-to-target').length){
-		$(".scroll-to-target").on('click', function() {
+	if ($('.scroll-to-target').length) {
+		$(".scroll-to-target").on('click', function () {
 			var target = $(this).attr('data-target');
-		   // animate
-		   $('html, body').animate({
-			   scrollTop: $(target).offset().top
-			 }, 1000);
-	
+			// animate
+			$('html, body').animate({
+				scrollTop: $(target).offset().top
+			}, 1000);
+
 		});
-	}	
+	}
 	// Elements Animation
-	if($('.wow').length){
+	if ($('.wow').length) {
 		var wow = new WOW(
-		  {
-			boxClass:     'wow',      // animated element css class (default is wow)
-			animateClass: 'animated', // animation css class (default is animated)
-			offset:       0,          // distance to the element when triggering the animation (default is 0)
-			mobile:       false,       // trigger animations on mobile devices (default is true)
-			live:         true       // act on asynchronously loaded content (default is true)
-		  }
+			{
+				boxClass: 'wow',      // animated element css class (default is wow)
+				animateClass: 'animated', // animation css class (default is animated)
+				offset: 0,          // distance to the element when triggering the animation (default is 0)
+				mobile: false,       // trigger animations on mobile devices (default is true)
+				live: true       // act on asynchronously loaded content (default is true)
+			}
 		);
 		wow.init();
 	}
 
-/* When document is Scrollig, do */
-	$(window).on('scroll', function() {
+	/* When document is Scrollig, do */
+	$(window).on('scroll', function () {
 		headerStyle();
 	});
-	
-/*When document is loaded, do */
-	$(window).on('load', function() {
+
+	/*When document is loaded, do */
+	$(window).on('load', function () {
 		handlePreloader();
-	});	
+	});
 
 })(window.jQuery);
 
 /* ================ Revolution Slider. ================ */
-	if($('.tp-banner').length > 0){
-		$('.tp-banner').show().revolution({
-			delay:6000,
-	        startheight:640,
-	        startwidth: 1170,
-	        hideThumbs: 1000,
-	        navigationType: 'none',
-	        touchenabled: 'on',
-	        onHoverStop: 'on',
-	        navOffsetHorizontal: 0,
-	        navOffsetVertical: 0,
-	        dottedOverlay: 'none',
-	        fullWidth: 'on'
-		});
-	}
-	if($('.tp-banner-full').length > 0){
-		$('.tp-banner-full').show().revolution({
-			delay:6000,
-	        hideThumbs: 1000,
-	        navigationType: 'none',
-	        touchenabled: 'on',
-	        onHoverStop: 'on',
-	        navOffsetHorizontal: 0,
-	        navOffsetVertical: 0,
-	        dottedOverlay: 'none',
-	        fullScreen: 'on'
-		});
-	}
-	  /*Masonry Function*/
-	var container_masonry = $(".container-masonry");
-	container_masonry.imagesLoaded(function() {
-		container_masonry.isotope({
-                    itemSelector: '.nf-item',
-                    transitionDuration: '1s',
-                    percentPosition: true,               
-                    masonry: {
-                    columnWidth: '.grid-sizer'
-                }
-                });
+if ($('.tp-banner').length > 0) {
+	$('.tp-banner').show().revolution({
+		delay: 6000,
+		startheight: 640,
+		startwidth: 1170,
+		hideThumbs: 1000,
+		navigationType: 'none',
+		touchenabled: 'on',
+		onHoverStop: 'on',
+		navOffsetHorizontal: 0,
+		navOffsetVertical: 0,
+		dottedOverlay: 'none',
+		fullWidth: 'on'
 	});
-
-	/*Masonry filter Function*/
-	var container_filter = $(".container-filter");
-	container_filter.on("click", ".categories", function() {
-		var a = $(this).attr("data-filter");
-		container_masonry.isotope({
-			filter : a
-		});
+}
+if ($('.tp-banner-full').length > 0) {
+	$('.tp-banner-full').show().revolution({
+		delay: 6000,
+		hideThumbs: 1000,
+		navigationType: 'none',
+		touchenabled: 'on',
+		onHoverStop: 'on',
+		navOffsetHorizontal: 0,
+		navOffsetVertical: 0,
+		dottedOverlay: 'none',
+		fullScreen: 'on'
 	});
-		
-	/*MAGNIFIC POPUP JS*/
-	        $('.video-play').magnificPopup({
-            type: 'iframe'
-        });
-        var magnifPopup = function () {
-            $('.work-popup').magnificPopup({
-                type: 'image',
-                removalDelay: 300,
-                mainClass: 'mfp-with-zoom',
-                gallery: {
-                    enabled: true
-                },
-                zoom: {
-                    enabled: true, // By default it's false, so don't forget to enable it
+}
+/*Masonry Function*/
+var container_masonry = $(".container-masonry");
+container_masonry.imagesLoaded(function () {
+	container_masonry.isotope({
+		itemSelector: '.nf-item',
+		transitionDuration: '1s',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-sizer'
+		}
+	});
+});
 
-                    duration: 300, // duration of the effect, in milliseconds
-                    easing: 'ease-in-out', // CSS transition easing function
+/*Masonry filter Function*/
+var container_filter = $(".container-filter");
+container_filter.on("click", ".categories", function () {
+	var a = $(this).attr("data-filter");
+	container_masonry.isotope({
+		filter: a
+	});
+});
 
-                    // The "opener" function should return the element from which popup will be zoomed in
-                    // and to which popup will be scaled down
-                    // By defailt it looks for an image tag:
-                    opener: function (openerElement) {
-                        // openerElement is the element on which popup was initialized, in this case its <a> tag
-                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                        return openerElement.is('img') ? openerElement : openerElement.find('img');
-                    }
-                }
-            });
-        };
-		
+/*MAGNIFIC POPUP JS*/
+$('.video-play').magnificPopup({
+	type: 'iframe'
+});
+var magnifPopup = function () {
+	$('.work-popup').magnificPopup({
+		type: 'image',
+		removalDelay: 300,
+		mainClass: 'mfp-with-zoom',
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function (openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+	});
+};
+
 // 3 LightBox / Fancybox
-if($('.lightbox-image').length) {
+if ($('.lightbox-image').length) {
 	$('.lightbox-image').fancybox({
-		openEffect  : 'elastic',
-		closeEffect : 'elastic',
-		helpers : {
-			media : {}
+		openEffect: 'elastic',
+		closeEffect: 'elastic',
+		helpers: {
+			media: {}
 		}
 	});
 }
 
-	
-		//testimonial Slider
-	if ($('.freelance-slider').length) {
-		$('.freelance-slider').owlCarousel({
-			loop:true,
-			margin:0,
-			nav:true,
-			smartSpeed: 3000,
-			autoplay: 4000,
-			navText: [ '<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>' ],
-			responsive:{
-				0:{
-					items:1
-				},
-				400:{
-					items:1
-				},
-				600:{
-					items:1
-				},
-				800:{
-					items:2
-				},
-				1200:{
-					items:3
-				}
-			}
-		});    		
-	}
-/*testimonials */
-$(document).ready(function() { 
-  	$(".owl-carousel").owlCarousel({ 
-      
-	   loop:true,
-		margin:10,
-		nav:false,
-		responsiveClass:true,
-		responsive:{
-			0:{
-				items:1,
-				nav:true
+
+//testimonial Slider
+if ($('.freelance-slider').length) {
+	$('.freelance-slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		smartSpeed: 3000,
+		autoplay: 4000,
+		navText: ['<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>'],
+		responsive: {
+			0: {
+				items: 1
 			},
-			700:{
-				items:1,
-				nav:false
+			400: {
+				items: 1
 			},
-			1170:{
-				items:1,
-				nav:true,
-				loop:false
+			600: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1200: {
+				items: 3
 			}
 		}
-	  
-	  
-  	}); 
-	});	
+	});
+}
+/*testimonials */
+$(document).ready(function () {
 	
-	
-	
-	/*==== Owl Carousel ====*/
-    var owl = $(".testimonialsList");
-      owl.owlCarousel({
-        autoPlay: 6000,
-        items : 1,
-        itemsDesktop : [1199,1],
-        itemsDesktopSmall : [979,1],
-        itemsTablet: [768,1],
-        itemsTabletSmall: false,
-        itemsMobile : [479,1],
-        Speedfast: 200,
-    });
-	
-	
-	
-	
-	
-	
-	//faqs Box
-	if($('.faqs-box').length){
-		$(".faqs-box").on('click', '.faqs-btn', function() {
-			
-			var target = $(this).parents('.faqs');
-			
-			if($(this).hasClass('active')!==true){
-				$('.faqs .faqs-btn').removeClass('active');
+
+
+	$(".owl-carousel").owlCarousel({
+
+		loop: true,
+		margin: 10,
+		nav: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			},
+			700: {
+				items: 1,
+				nav: false
+			},
+			1170: {
+				items: 1,
+				nav: true,
+				loop: false
 			}
-			
-			if ($(this).next('.faqs-content').is(':visible')){
-				//$(this).removeClass('active');
-				return false;
-				//$(this).next('.accord-content').slideUp(300);
-			}else{
-				$(this).addClass('active');
-				$('.faqs').removeClass('active-block');
-				$('.faqs .faqs-content').slideUp(300);
-				target.addClass('active-block');
-				$(this).next('.faqs-content').slideDown(300);	
-			}
-		});	
-	}
+		}
+
+
+	});
+});
+
+
+
+/*==== Owl Carousel ====*/
+var owl = $(".testimonialsList");
+owl.owlCarousel({
+	autoPlay: 6000,
+	items: 1,
+	itemsDesktop: [1199, 1],
+	itemsDesktopSmall: [979, 1],
+	itemsTablet: [768, 1],
+	itemsTabletSmall: false,
+	itemsMobile: [479, 1],
+	Speedfast: 200,
+});
+
+
+
+
+
+
+
+
+//faqs Box
+if ($('.faqs-box').length) {
+	$(".faqs-box").on('click', '.faqs-btn', function () {
+
+		var target = $(this).parents('.faqs');
+
+		if ($(this).hasClass('active') !== true) {
+			$('.faqs .faqs-btn').removeClass('active');
+		}
+
+		if ($(this).next('.faqs-content').is(':visible')) {
+			//$(this).removeClass('active');
+			return false;
+			//$(this).next('.accord-content').slideUp(300);
+		} else {
+			$(this).addClass('active');
+			$('.faqs').removeClass('active-block');
+			$('.faqs .faqs-content').slideUp(300);
+			target.addClass('active-block');
+			$(this).next('.faqs-content').slideDown(300);
+		}
+	});
+}
 
 
 
 
 /*----Counter-------*/
 
- $('.counter-item').appear(function() {
- $('.counter-number').countTo();
-    });
+$('.counter-item').appear(function () {
+	$('.counter-number').countTo();
+});
 
 
 
-	//brand-slider
-	if ($('.brand-slider').length) {
-		$('.brand-slider').owlCarousel({
-			loop:true,
-			margin:0,
-			nav:true,
-			smartSpeed: 3000,
-			autoplay: 4000,
-			navText: [ '<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>' ],
-			responsive:{
-				0:{
-					items:1
-				},
-				400:{
-					items:2
-				},
-				600:{
-					items:3
-				},
-				800:{
-					items:4
-				},
-				1200:{
-					items:5
-				}
+//brand-slider
+if ($('.brand-slider').length) {
+	$('.brand-slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		smartSpeed: 3000,
+		autoplay: 4000,
+		navText: ['<span class="fa fa-long-arrow-left"></span>', '<span class="fa fa-long-arrow-right"></span>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			400: {
+				items: 2
+			},
+			600: {
+				items: 3
+			},
+			800: {
+				items: 4
+			},
+			1200: {
+				items: 5
 			}
-		});    		
-	}
-	
-	
-	$(document).ready(function() {
-		$(".fancybox").fancybox();
+		}
 	});
-	
-	
-	
-	
+}
+
+
+$(document).ready(function () {
+	$(".fancybox").fancybox();
+});
+
+
+
+function showSection(id) {
+	// Hide all sections
+	const sections = document.querySelectorAll('.section');
+	sections.forEach(section => {
+		section.style.display = 'none';
+	});
+
+	// Show selected section
+	document.getElementById(id).style.display = 'block';
+}
+
+
